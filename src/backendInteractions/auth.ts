7f014548f2fe9cf9a7d45239056552998b07cb7c
Thunman <thunman42@gmail.com>
2024-03-15 15:05:45 +0100

    login function update

--- src/backendInteractions/auth.ts
+++ src/backendInteractions/auth.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 export const login = async (username: string, password: string) => {
-  const response = await axios.post("thunman.servebeer.com/login", {
-    username,
-    password,
-  });
+  const response = await axios.post(
+    "https://thunman.servebeer.com/api/users/login",
+    {
+      username: username,
+      password: password,
+    }
+  );
   console.log(response);
   return response;
 };
